8a71ffdc2ae99da42f087a588b87ebfd0456a71b
dasilvaca <dasilvaca@unal.edu.co>
2023-08-21 12:51:16 -0500

    Finished game logic. Now I'm making the views

--- reto1/TicTacToeGame/app/src/main/java/com/dasilvaca/tictactoegame/TicTacToeGame.java
+++ reto1/TicTacToeGame/app/src/main/java/com/dasilvaca/tictactoegame/TicTacToeGame.java
@@ -7,6 +7,7 @@ package com.dasilvaca.tictactoegame;
  * is X and the computer is O.
  */
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class TicTacToeGame {
@@ -27,16 +28,14 @@ private static class Coordinate{
         OPEN_SPOT,
     }
 
-    private char[] mBoard = {'1','2','3','4','5','6','7','8','9'};
 
-    private Symbol[][] aBoard = new Symbol[3][3];
+    private final Symbol[][] mBoard = new Symbol[3][3]; //! I made it final, but I suspect it is not
     private static final int ROWS = 3;
     private static final int COLUMNS = 3;
 
     public static final Symbol HUMAN_PLAYER = Symbol.X;
     public static final Symbol COMPUTER_PLAYER = Symbol.O;
 
-    public static final char OPEN_SPOT = ' ';
 
     private final Random mRand;
 
@@ -46,7 +45,7 @@ private static class Coordinate{
     public void clearBoard() {
         for(int i = 0; i < ROWS; i++){
             for(int j = 0; j < COLUMNS; j++) {
-                aBoard[i][j] = Symbol.OPEN_SPOT;
+                mBoard[i][j] = Symbol.OPEN_SPOT;
             }
         }
     }
@@ -59,7 +58,7 @@ private static class Coordinate{
      * @param location - The Coordinates corresponding to location (i,j) to place the move
      */
     public void setMove(Symbol player, Coordinate location) {
-        aBoard[location.i][location.j] = player;
+        mBoard[location.i][location.j] = player;
     }
 
     /**
@@ -68,9 +67,49 @@ private static class Coordinate{
      *
      * @return The best move for the computer to make (0-8).
      */
-    public int getComputerMove() {
+    public Coordinate getComputerMove() {
 
-        return 0;
+        // Check if in a single move O could win
+        for(int i = 0; i < ROWS; i ++){
+            for(int j = 0; j < COLUMNS; j++){
+                if(mBoard[i][j].equals(Symbol.OPEN_SPOT)){
+                    Coordinate possibleMove = new Coordinate(i,j);
+                    setMove(COMPUTER_PLAYER, possibleMove);
+                    if(checkForWinner() == 3){
+                        return possibleMove;
+                    }
+                }
+            }
+        }
+
+        // Check if can prevent X to win
+        for(int i = 0; i < ROWS; i ++){
+            for(int j = 0; j < COLUMNS; j++){
+                if(mBoard[i][j].equals(Symbol.OPEN_SPOT)){
+                    Coordinate possibleMove = new Coordinate(i,j);
+                    setMove(HUMAN_PLAYER, possibleMove);
+                    if(checkForWinner() == 2){
+                        setMove(COMPUTER_PLAYER, possibleMove);
+                        return possibleMove;
+                    }
+                }
+            }
+        }
+
+        // Make a random move
+        // Keep the free possible movements
+        ArrayList<Coordinate> possibleMoves = new ArrayList<>();
+        for (int i = 0; i < ROWS; i++){
+            for(int j = 0; j < COLUMNS; j++){
+                if(mBoard[i][j].equals(Symbol.OPEN_SPOT)){
+                    possibleMoves.add(new Coordinate(i,j));
+                }
+            }
+        }
+        int moveSelected = mRand.nextInt(possibleMoves.size());
+        setMove(COMPUTER_PLAYER, possibleMoves.get(moveSelected));
+        return possibleMoves.get(moveSelected);
+
     }
 
     /**
@@ -82,22 +121,22 @@ private static class Coordinate{
     public int checkForWinner() {
         // Checks for a winner at rows and columns
         for(int i = 0; i < ROWS; i++){
-            if(!aBoard[i][0].equals(Symbol.OPEN_SPOT) &&
-            aBoard[i][0].equals(aBoard[i][1]) &&
-            aBoard[i][1].equals(aBoard[i][2])
+            if(!mBoard[i][0].equals(Symbol.OPEN_SPOT) &&
+            mBoard[i][0].equals(mBoard[i][1]) &&
+            mBoard[i][1].equals(mBoard[i][2])
             ){
-                return aBoard[i][0].equals(Symbol.X)?2:3;
+                return mBoard[i][0].equals(Symbol.X)?2:3;
             }
-            else if(!aBoard[0][i].equals(Symbol.OPEN_SPOT) &&
-                    aBoard[0][i].equals(aBoard[1][i]) &&
-                    aBoard[1][i].equals(aBoard[2][i])
+            else if(!mBoard[0][i].equals(Symbol.OPEN_SPOT) &&
+                    mBoard[0][i].equals(mBoard[1][i]) &&
+                    mBoard[1][i].equals(mBoard[2][i])
             ){
-                return aBoard[0][i].equals(Symbol.X)?2:3;
+                return mBoard[0][i].equals(Symbol.X)?2:3;
             }
         }
         // Checks for a winner on diagonals
-        Symbol mainDiagonalSymbol = aBoard[0][0];
-        Symbol secondaryDiagonalSymbol = aBoard[0][ROWS - 1];
+        Symbol mainDiagonalSymbol = mBoard[0][0];
+        Symbol secondaryDiagonalSymbol = mBoard[0][ROWS - 1];
         // We say that, since every Diagonal starts with a non empty cell,
         // The symbol on it is a winner until the opposite is demonstrated
         boolean mainDiagonalWinner = !(mainDiagonalSymbol.equals(Symbol.OPEN_SPOT));
@@ -105,10 +144,10 @@ private static class Coordinate{
         for(int i = 0; i < ROWS && (mainDiagonalWinner || secondaryDiagonalWinner); i++){
             for(int j = 0; j < COLUMNS; j++){
                 if(i == j && mainDiagonalWinner) {
-                    if(!(aBoard[i][j].equals(mainDiagonalSymbol))) mainDiagonalWinner = false;
+                    if(!(mBoard[i][j].equals(mainDiagonalSymbol))) mainDiagonalWinner = false;
                 }
                 else if(i == ((ROWS - 1) - j) && secondaryDiagonalWinner){
-                    if(!(aBoard[i][j].equals(secondaryDiagonalSymbol))) secondaryDiagonalWinner = false;
+                    if(!(mBoard[i][j].equals(secondaryDiagonalSymbol))) secondaryDiagonalWinner = false;
                 }
             }
         }
@@ -118,7 +157,7 @@ private static class Coordinate{
         // Check for a tie
         for(int i = 0; i < ROWS; i++){
             for (int j = 0; j < COLUMNS; j++){
-                if(aBoard[i][j].equals(Symbol.OPEN_SPOT)) return  0;
+                if(mBoard[i][j].equals(Symbol.OPEN_SPOT)) return  0;
             }
         }
         return 1;
@@ -131,15 +170,6 @@ private static class Coordinate{
 
     }
 
-    private void displayBoard()	{
-        System.out.println();
-        System.out.println(mBoard[0] + " | " + mBoard[1] + " | " + mBoard[2]);
-        System.out.println("-----------");
-        System.out.println(mBoard[3] + " | " + mBoard[4] + " | " + mBoard[5]);
-        System.out.println("-----------");
-        System.out.println(mBoard[6] + " | " + mBoard[7] + " | " + mBoard[8]);
-        System.out.println();
-    }
 
 }
 
